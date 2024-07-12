e058efd216a71ea29a594a55eeec1e760ba01224
sooyeon-kr <sooyeon7027@naver.com>
2024-07-12 23:32:59 +0900

    chore: 구현해야 할 기능을 주석으로 작성

--- src/restaurant/RestaurantController.java
+++ src/restaurant/RestaurantController.java
@@ -11,4 +11,12 @@ public class RestaurantController {
         return restaurantService.openRestaurant(name, menus);
     }
 
+//    레스토랑 리스트 조회
+
+//    하나의 레스토랑 조회
+
+//    하나의 레스토랑에 해당하는 메뉴들 조회
+
+//    주문받기
+
 }
